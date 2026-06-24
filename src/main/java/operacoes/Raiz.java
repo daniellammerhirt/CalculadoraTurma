1b93d869d74547a7d783c2a04cd408c674d1a436
Nicolly Mazeika <nicollydepaula10@gmail.com>
2026-06-24 14:09:35 -0300

    implmenta Raiz a calculadora

--- src/main/java/operacoes/Raiz.java
+++ src/main/java/operacoes/Raiz.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Raiz {
     static void main() {
         Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
+        System.out.println("digite o numero que deseja a raiz");
+        double x = scanner.nextDouble();
+        System.out.println("o resultado é " + calcularRaiz(x));
 
-        System.out.println(Math.sqrt(x));
+    }
+    public static double calcularRaiz (double x){
+        return Math.sqrt(x);
     }
 }
