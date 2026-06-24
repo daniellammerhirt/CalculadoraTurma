c50952cdfb2afdf1ba4eca5dc7a685b24b2dc9c4
Nicolly Mazeika <nicollydepaula10@gmail.com>
2026-06-24 14:38:01 -0300

    chor: coloca raiz dentro do pradrão da calculadora

--- src/main/java/operacoes/Raiz.java
+++ src/main/java/operacoes/Raiz.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Raiz {
     static void main() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("digite o numero que deseja a raiz");
-        double x = scanner.nextDouble();
-        System.out.println("o resultado é " + calcularRaiz(x));
+        double x;
+        System.out.println("Digite o primeiro valor: ");
+        x = scanner.nextDouble();
+        System.out.println("o resultado é " + calcular(x));
 
     }
-    public static double calcularRaiz (double x){
+    public static double calcular(double x){
         return Math.sqrt(x);
     }
 }
