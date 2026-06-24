967107dcd4938ddd29a4a855df0b154eab6cde9e
Mike <michaelbue04@gmail.com>
2026-06-24 14:37:53 -0300

    chore: altera padrao de estruturacao de codigo

--- src/main/java/operacoes/Subtracao.java
+++ src/main/java/operacoes/Subtracao.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Subtracao {
     static void main() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o primeiro numero");
-        double x = scanner.nextDouble();
-        System.out.println("Digite o segundo numero");
-        double y = scanner.nextDouble();
-        System.out.println("O resultado eh: " + calcular(x,y));
+        double x;
+        double y;
+        System.out.println("Digite o primeiro valor: ");
+        x = scanner.nextDouble();
+        System.out.println("Digite o segundo valor: ");
+        y = scanner.nextDouble();
+        System.out.println("O resultado é: " + calcular(x, y));
     }
 
     public static double calcular(double x, double y){
