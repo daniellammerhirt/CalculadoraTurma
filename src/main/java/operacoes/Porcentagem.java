06b3d93e6c4c4ffff384e03f106322e20a123f6e
Maria Eduarda de andrade Schechtel <duda.schechtel@gmail.com>
2026-06-25 16:15:51 -0300

    refactor: porcentagem usando as outras classes criadas.

--- src/main/java/operacoes/Porcentagem.java
+++ src/main/java/operacoes/Porcentagem.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class Porcentagem {
     static void main() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o valor");
+        System.out.println("Digite o primeiro valor: ");
         double x = scanner.nextDouble();
-        System.out.println("Digite a porcentagem do valor");
+        System.out.println("Digite a porcentagem do valor: ");
         double y = scanner.nextDouble();
-        System.out.println("O resultado eh: " + calcular(x,y));
+        System.out.println("O resultado é: " + calcular(x,y));
     }
 
     public static double calcular(double x, double y){
-        return (x*y)/100;
+        return Divisao.calcular(Multiplicacao.calcular(x, y), 100);
     }
 }
