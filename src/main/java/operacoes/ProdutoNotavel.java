21dae5f8fc2de5ca8232f71f0947901520723f9e
Maria Eduarda de andrade Schechtel <duda.schechtel@gmail.com>
2026-07-01 13:33:34 -0300

    feat: implementa a função de produto notável na calculadora

---
+++ src/main/java/operacoes/ProdutoNotavel.java
@@ -0,0 +1,18 @@
+package operacoes;
+import java.util.Scanner;
+
+public class ProdutoNotavel {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        double x;
+        double y;
+        System.out.println("Digite o primeiro valor: ");
+        x = scanner.nextDouble();
+        System.out.println("Digite o segundo valor: ");
+        y = scanner.nextDouble();
+        System.out.println("o resultado é: " + calcular(x,y));
+    }
+    public static double calcular(double x, double y){
+        return Potencia.calcular(Soma.calcular(x, y),2);
+    }
+}
